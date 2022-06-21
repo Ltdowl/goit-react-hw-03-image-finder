6baf64804bb73d21b03e29a92ab6d3a39c82072a
Ltdowl <sashag12s@gmail.com>
2022-06-21 18:07:44 +0300

    переставив onClick

--- src/components/ImageGallery/ImageGalleryItem.jsx
+++ src/components/ImageGallery/ImageGalleryItem.jsx
@@ -3,14 +3,13 @@ import s from './ImageGallery.module.css';
 
 export const ImageGalleryItem = ({ images, onClick }) => {
   return images.map(image => (
-    <li key={image.id}
-      className={s.ImageGalleryItem}
-      onClick={onClick}>
+    <li key={image.id} className={s.ImageGalleryItem}>
       <img
         src={image.imageURL}
         alt={image.tags}
         className={s.ImageGalleryItemImage}
         name={image.src}
+        onClick={onClick}
       />
     </li>
   ));
